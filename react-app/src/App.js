7b33dfa2f7bfd6e23a49a53d251c6a81728d0c25
Dylan Durand <dylan.durand88@outlook.fr>
2023-01-13 19:32:26 +0100

    Gestion des conditions avec les fonctions ternaires

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -10,35 +10,12 @@ function App() {
   const changeState = () => {
     setToggle(!toggle);
   }
-// Version 1
-  // if(toggle) {
-  //   return (
-  //     <div className="App">
-  //       <h1>Le state est True</h1>
-  //       <button onClick={changeState}>Changer de State</button>
-  //     </div>
-  //   );
-  // } else if(toggle === false) {
-  //     return (
-  //       <div className="App">
-  //         <h1>Le state est False</h1>
-  //         <button onClick={changeState}>Changer de State</button>
-  //       </div>
-  //     );
-  // }
-
-  // Version 2
-  let toggleContenu;
-
-  if(toggle) {
-    toggleContenu = <h1>Le state est True</h1>
-  } else {
-    toggleContenu = <h1>Le state est False</h1>
-  }
 
   return (
     <div className="App">
-      {toggleContenu}
+      {/* Fonction ternaire */}
+      {toggle ? <h1>Le state est True</h1> :
+        <h1>Le state est False</h1>}
       <button onClick={changeState}>Changer de State</button>
     </div>
   );
