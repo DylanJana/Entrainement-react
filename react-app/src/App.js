85dc6b0e050a08dcf33bedd9a6df2e3b23cf2606
Dylan Durand <dylan.durand88@outlook.fr>
2023-01-13 17:40:40 +0100

    Ajout du css dans react

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,21 +1,12 @@
 // Il n'est plus nécessaire d'importer react
-// La méthode useState permet de gérer les states des composants
-import {useState} from 'react';
-import Item from './Item';
+import Item from './Components/Item/Item.js';
 
 function App() {
-  // J'initialise mon state
-  const [monState, setMonState] = useState(10);
-  // Je crée la fonction liée à mon event click du button change state
-  // Elle prendra des datas en argument
-  const modifyState = (data) => {
-    // console.log(data);
-    setMonState(data);
-  }
+
   return (
     <div className="App">
-      <h1>Hello State : {monState}</h1>
-      <Item func={modifyState} />
+      <h1>Hello depuis App</h1>
+      <Item />
     </div>
   );
 }
---
+++ react-app/src/Components/Item/Item.js
@@ -0,0 +1,11 @@
+import './item.css';
+function Item() {
+
+  return (
+    <div>
+      <h1 className='titre--xxl'>Hello depuis Item</h1>
+    </div>
+  )
+}
+
+export default Item;
