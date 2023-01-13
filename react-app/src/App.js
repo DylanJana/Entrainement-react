d5f36a1925ca86cb2d304130a5bbb095f015f496
Dylan Durand <dylan.durand88@outlook.fr>
2023-01-13 12:55:54 +0100

    Création d'un component
    Utilisation du State

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,7 +1,16 @@
+// Il n'est plus nécessaire d'importer react
+// La méthode useState permet de gérer les states des composants
+import {useState} from 'react';
+import Item from './Item';
+
 function App() {
+  // J'initialise mon state
+  const [monState, setMonState] = useState(10);
   return (
     <div className="App">
       <h1>Hello app react</h1>
+      <Item />
+      {monState}
     </div>
   );
 }
