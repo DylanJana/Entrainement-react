4bc06c51379e1216dab1b05a1502ce10df1fb374
Dylan Durand <dylan.durand88@outlook.fr>
2023-01-13 18:25:38 +0100

    Mise en place d'image et de svg dans un projet

--- react-app/src/Components/Item/Item.js
+++ react-app/src/Components/Item/Item.js
@@ -1,9 +1,19 @@
 import './item.css';
+import Cat from './happy.svg';
 function Item() {
 
   return (
     <div>
       <h1 className='titre--xxl'>Hello depuis Item</h1>
+      <img 
+        src={"assets/img/chiot.jpg"} 
+        alt="Image d'un chien" 
+      />
+      {/* Icône svg d'un chat */}
+      <img 
+        src={Cat} 
+        alt="Icône de chat" 
+      />
     </div>
   )
 }
