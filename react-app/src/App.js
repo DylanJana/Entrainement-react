c6e14696c216e7a6cfc9632d1f9535be6d30ab8a
Dylan Durand <dylan.durand88@outlook.fr>
2023-01-13 20:09:06 +0100

    Exemple de création de liste avec react

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,20 +5,17 @@ import Item from './Components/Item/Item.js';
 
 function App() {
 
-  const [toggle, setToggle] = useState(true);
-
-  const changeState = () => {
-    setToggle(!toggle);
-  }
+  const [dataArr, setDataArr] = useState([
+    {nom: 'Juliette'},
+    {nom: 'John'},
+    {nom: 'Joris'}
+  ]);
 
   return (
     <div className="App">
-      {/* Toggle la classe */}
-      <div className= {toggle ? "box animated" : "box"}></div>
-      {/* Toggle le style inline peut utiliser car lourd */}
-      {/* <div className="box" style={{ backgroundColor: toggle ? "salmon" : "blue" }}>
-      </div> */}
-      <button onClick={changeState}>Changer de State</button>
+      {dataArr.map((item, index) => {
+        return <p key={index}>Nom : {item.nom}</p>
+      })}
     </div>
   );
 }
