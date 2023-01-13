e1c8766c1963f32e8c8f211ab4244d034030d700
Dylan Durand <dylan.durand88@outlook.fr>
2023-01-13 19:11:39 +0100

    Input et changement de valeur
    Je peux définir une valeur par défaut de mon input et la modifiée selon le state

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,25 @@
 // Il n'est plus nécessaire d'importer react
+import { useState } from 'react';
 import Item from './Components/Item/Item.js';
+
 
 function App() {
 
+  const [inputData, setInputData] = useState(10);
+
+  const changeInput = (e) => {
+    setInputData(e)
+  }
+
+  console.log(inputData)
   return (
     <div className="App">
       <h1>Hello depuis App</h1>
-      <Item />
+      <input 
+      type="text"
+      value={inputData}
+      onInput={e => changeInput(e.target.value)}
+      />
     </div>
   );
 }
