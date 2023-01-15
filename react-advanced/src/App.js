2b05615f2fb82671fee8d6ca0bcb487ddcbdec25
Dylan Durand <dylan.durand88@outlook.fr>
2023-01-15 16:31:47 +0100

    Requête à une API avec Fetch
    Si, la requête est un succès et que j'ai la donnée, alors création d'une image

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -3,23 +3,39 @@ import './App.css';
 
 function App() {
 
-  const [dataComponent, setDataComponent] = useState(1);
+  const [dataImg, setDataImg] = useState();
 
-  // useEffect
   useEffect(() => {
-    console.log("Premier affichage")
-  }, [dataComponent])
-
-  const changeState = () => {
-    setDataComponent(dataComponent + 1)
-  }
-
+    // Méthode qui permet de faire appel à des API
+    // fetch veut dire, aller chercher
+    fetch('https://api.thecatapi.com/v1/images/search')
+    /* Si l'appel est un succès, je recois 
+    une réponse HTTP*/
+      .then(response => {
+        console.log(response);
+        // Retourne un tableau avec un objet
+        // dedans
+        return response.json();
+      })
+      /* Alors si la requète http est un succès
+      alors donne moi les données */
+      .then(data => {
+        // Dans le premier objet prend url
+        setDataImg(data[0].url);
+      })
+  }, []);
+  
   return (
     <div className="App">
-      <div>
-      <h1>Le state est {dataComponent}</h1>
-      <button onClick={() => changeState()}>Change State</button>
-      </div>
+      {/*Lorsque j'ai reçu les données dataImg est
+      true dans ce cas affiche moi l'image du chat
+      avec une taill de 500px*/}
+      {dataImg && 
+      <img 
+      src={dataImg} 
+      alt='cat image'
+      style={{width: '500px'}} 
+      />}
     </div>
   );
 }
