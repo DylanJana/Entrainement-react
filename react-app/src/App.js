8b8779b0e1a2bf1d6614c05dafaea22db94b127b
Dylan Durand <dylan.durand88@outlook.fr>
2023-01-15 11:37:30 +0100

    Layout de la todo list

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,13 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// Import du formulaire
+import Form from './Components/Form';
 
 function App() {
 
   return (
     <div className="App">
+      <h1 className="text-center mt-3">Todo-List</h1>
+      <Form />
     </div>
   );
 }
