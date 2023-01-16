30bc114795cac5e1316b41b393a299a9a1545417
Dylan Durand <dylan.durand88@outlook.fr>
2023-01-16 20:56:43 +0100

    Ne jamais modifier le state directement
    Ceci est obligatoire lorsque l'on utilise des objets ou même des tableaux

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,70 +1,21 @@
-import { useState, useEffect, useRef } from 'react';
-import { ref } from 'vue';
+import { useState } from 'react';
 import './App.css';
-// Import de la vidéo téléchargé en local
-import Video from './Video.mp4';
+
 
 function App() {
 
-  const[toggle, setToggle] = useState(true);
+  const[toggle, setToggle] = useState([1,2,3]);
 
-  const maRef = useRef([]);
-
-  useEffect(() => {
-    // Lorsque je vais redimensionner ma fenête
-    window.addEventListener('resize', actionResize);
-
-    function actionResize() {
-      console.log("Resized !!!")
-    }
-  }, [])
 
   const toggleFunc = () => {
-    setToggle(!toggle);
-  }
-
-  /* Le el en paramètre, s'agit de l'élément d'où
-  est appelé la méthode
-  */
-  const addToRef = el => {
-    /* Si l'élément sélectionné (mon élément du Dom)
-    n'est pas présent dans mon tableau maRef dans ce
-    cas je le push à l'intérieur
-    */
-    if(el && !maRef.current.includes(el)) {
-      maRef.current.push(el);
-    }
+    const newArr= [...toggle]
+    newArr.push(4);
+    setToggle(newArr);
   }
 
   return (
     <div className="App">
-      <video 
-      width='750px' 
-      height='500' 
-      autoPlay 
-      controls 
-      muted
-      ref={addToRef}>
-        <source src={Video} type='video/mp4/' />
-      </video>
-      <video 
-      width='750px' 
-      height='500' 
-      autoPlay 
-      controls 
-      muted
-      ref={addToRef}>
-        <source src={Video} type='video/mp4/' />
-      </video>
-      <video 
-      width='750px' 
-      height='500' 
-      autoPlay 
-      controls 
-      muted
-      ref={addToRef}>
-        <source src={Video} type='video/mp4/' />
-      </video>
+      <h1>{toggle}</h1>
       <button onClick={toggleFunc}>Toggle</button>
     </div>
   );
