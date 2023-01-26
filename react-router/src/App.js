4e0c855b3cdcc4aa7d747d515fdc31186ae02d6d
DylanJana <dylan@janaconcierge.com>
2023-01-26 20:09:23 +0100

    Gestion des routes imbriquées

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,15 +1,26 @@
 import {Routes, Route} from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Components/Home";
 import Profile from "./Components/Profile/Profile";
+import Services from "./Components/Services/Services";
 import Error404 from "./Components/Error404/Error404";
 import './App.css';
+import Contact from "./Components/Contact/Contact";
+import Development from "./Components/Services/Development/Development";
+import CyberSecurity from "./Components/Services/CyberSecurity/CyberSecurity";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profil/:id" element={<Profile />} />
+        <Route path="/services" element={<Services />}>
+          <Route path="/services/developpement" element={<Development />}/>
+          <Route path="/services/cybersecurite" element={<CyberSecurity />}/>
+        </Route>
+        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </div>
---
+++ react-router/src/Components/Services/Development/Development.js
@@ -0,0 +1,13 @@
+function Development() {
+  return (
+    <div>
+      <h2>Nos services de développement</h2>
+      <ul>
+        <li>Frontend</li>
+        <li>Backend</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Development;
