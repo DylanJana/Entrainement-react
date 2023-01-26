fc85e659ee4b3879569ddc3016e5db9dd834e30a
DylanJana <dylan@janaconcierge.com>
2023-01-26 18:26:26 +0100

    Création de chemin dynamique + useNavigate

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,5 +1,7 @@
 import {Routes, Route} from "react-router-dom";
 import Home from "./Components/Home";
+import Profile from "./Components/Profile/Profile";
+import Error404 from "./Components/Error404/Error404";
 import './App.css';
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/profil/:id" element={<Profile />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </div>
   );
---
+++ react-router/src/Components/Profile/Profile.js
@@ -0,0 +1,11 @@
+import './Profile.css';
+
+function Profile() {
+  return (
+    <div>
+      <h1>Bienvenue sur votre profil.</h1>
+    </div>
+  )
+}
+
+export default Profile;
