8b1c860e4e496604b438d44e4e26802ff89a4390
DylanJana <dylan@janaconcierge.com>
2023-01-26 20:21:33 +0100

    Utilisation des hooks useParams et useLocation

--- react-router/src/Components/Contact/Contact.js
+++ react-router/src/Components/Contact/Contact.js
@@ -1,6 +1,11 @@
 import './Contact.css';
+import { useLocation } from 'react-router-dom';
 
 function Contact() {
+  // Get more info on my page like 
+  // pathname, search, hash, state etc...
+  const location = useLocation();
+  console.log(location)
   return (
     <div>
       <h1>Contact</h1>
--- react-router/src/Components/Home.js
+++ react-router/src/Components/Home.js
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div>
       <h1>Bienvenue sur le site</h1>
+      <Link 
+      to="/contact"
+      state={"test"}
+      >Contact</Link>
     </div>
   )
 }
