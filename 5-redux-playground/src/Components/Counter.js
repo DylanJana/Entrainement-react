d7676f1de9c45cf1b2c55eae04311fdf4838b06a
Dylan Durand <dylan.durand88@outlook.fr>
2023-04-07 17:57:45 +0200

    Reacr redux en profondeur

--- 5-redux-playground/src/Components/Counter.js
+++ 5-redux-playground/src/Components/Counter.js
@@ -1,16 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import {useSelector, useDispatch} from 'react-redux';
 import AddCartReducer from '../redux/reducers/addCartReducer';
+import { getCatImg } from '../redux/reducers/dataImgReducer';
 
 export default function Counter() {
   //Creation du state
   const [cartData, setCartData] = useState(0);
   // Je vais récupérer les INITIAL_STATE de chaque reducer
-  const {cart, count} = useSelector(state => ({
+  const {cart, count, imgUrl} = useSelector(state => ({
     // J'initialise la constante cart avec le state de AddCartReducer
     ...state.AddCartReducer,
     //J'initialise la constante count avec le state de CounterReducer
-    ...state.CounterReducer
+    ...state.CounterReducer,
+    // Je récupére les données de getCatImg
+    ...state.dataImgReducer
   }))
 
   // J'instancie useDispatch pour le réutiliser ensuite
@@ -38,6 +41,11 @@ export default function Counter() {
     })
   }
 
+  useEffect(() => {
+    // A la fin d'affichage de mon composant j'envoi un dispatch
+    dispatch(getCatImg())
+  }, [])
+
   return (
     <div>
       <h1>Votre panier : {cart} {count}</h1>
@@ -55,6 +63,13 @@ export default function Counter() {
       >
         Ajouter au panier
       </button>
+      {/* Quand j'ai reçu mon img alors je crée une balise img avec la source de l'images
+      récupérée */}
+      <div>
+      {imgUrl && <img style={{
+        width: '300px'
+      }} src={imgUrl} />}
+      </div>
     </div>
   )
 }
--- 5-redux-playground/src/redux/store.js
+++ 5-redux-playground/src/redux/store.js
@@ -2,36 +2,17 @@
 import {createStore, combineReducers, applyMiddleware } from "redux";
 import CounterReducer from './reducers/counterReducer';
 import AddCartReducer from './reducers/addCartReducer';
+import dataImgReducer from "./reducers/dataImgReducer";
+import thunk from 'redux-thunk';
 
 
 //Combine reducers
 const rootReducer = combineReducers({
   CounterReducer,
-  AddCartReducer
+  AddCartReducer,
+  dataImgReducer
 })
-
-// Je met en place mon middleware
-const customMiddleware = store => next => action => {
-  // Dans le store, je peux avoir dispatch pour changer le dispatch et getState 
-  // Pour obtenir le state
-  console.log("store ", store.getState());
-
-  // next est une fonction, on l'appelle lorsque l'on a fini le taf dans le middleware
-  // Son objectif est de faire continuer le chemin vers le dispatch (effectuer le dispatch)
-  console.log("next ", next)
-
-  const actionModif = {
-    type: "ADDTOCART",
-    payload: 789
-  }
-
-  next(actionModif)
-  console.log("NEXT OK ")
-  // Action est l'action de notre dispatch
-  // Dans notre cas ADDCART avec le payload 0
-  console.log(action);
-}
 // Je crée mon store
-const store = createStore(rootReducer, applyMiddleware(customMiddleware));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
